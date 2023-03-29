4959c7b53720065847f200e0846c413cd893c2be
Valentin Mairesse <contact@mairs.fr>
2023-03-29 14:32:21 +0200

    feature(Products): views & controllers

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProduitController extends AbstractController
 {
-    #[Route('/produit', name: 'app_produit')]
-    public function index(): Response
+    #[Route('/produits', name: 'app_produits')]
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $repository = $doctrine->getRepository(Produit::class);
+        $produits = $repository->findAll();
         return $this->render('produit/index.html.twig', [
-            'controller_name' => 'ProduitController',
+            'produits' => $produits,
+        ]);
+    }
+
+    #[Route('/produit/{id}', name: 'app_produit')]
+    public function getProduitById(ManagerRegistry $doctrine, $id): Response
+    {
+        $repository = $doctrine->getRepository(Produit::class);
+        $produit = $repository->find($id);
+        return $this->render('produit/product.html.twig', [
+            'produit' => $produit,
         ]);
     }
 }
